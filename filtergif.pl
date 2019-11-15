f316e5492d68301f92663b13878d26446ecae3c6
ebeatty1 <40609703+ebeatty1@users.noreply.github.com>
2019-11-14 22:42:36 -0500

    Fix for BASH limitation

--- filtergif.pl
+++ filtergif.pl
@@ -2,21 +2,37 @@
 use warnings; use strict; use File::Copy;
 use Image::ExifTool qw(:Public);
 
-# Takes a bunch of .gif files as input and sorts them into new directories based on whether or not the files contain more 
-# than a single frame. Requires Image::ExifTool (install with cpan or cpanm) and File::Copy, which should be installed by 
-# default.
+# Takes a bunch of .gif files as input and sorts them into new directories based on whether or 
+# not the files contain more than a single frame. Requires Image::ExifTool (install with cpan 
+# or cpanm) and File::Copy, which should be installed by default.
 
-my $usage = "perl filtergif.pl ./*\n";
+my $usage = "perl filtergif.pl ./\n";
 die $usage unless @ARGV;
 
+my $directory = shift (@ARGV);
 system "mkdir -p ./animated && mkdir -p ./static";
 
-while (my $file = shift (@ARGV)) {
-    my $info = ImageInfo($file);
+print "Identifying \.gif\'s\n";
+open my $out, ">", "./list.temp" or die "Can't open list.temp\n";
+print $out `find $directory -type f -regex ".*\.gif\$"`;
+close $out;
+
+my $filecount = `grep -c -P "\.gif\$" ./list.temp`;
+chomp $filecount;
+print "There are $filecount \.gif\'s in $directory";
+
+open my $in, "<", "./list.temp" or die "Can't open list.temp\n";
+print "Moving all $filecount \.gif\'s";
+
+while (my $line = <$in>) {
+    chomp ($line);
+    my $info = ImageInfo($line);
 
     if (exists ($$info{FrameCount}) && $$info{FrameCount} >= 1) { 
-        move ("$file", './animated/');
+        copy ("$line", './animated/');
         next;
     } 
-    move ("$file", './static/');
+    copy ("$line", './static/');
 }
+close $in;
+system "rm ./list.temp";
